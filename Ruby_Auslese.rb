55979b329b16d48cc0b7789befd1dd7971677678
pygoner <pygoner@gmail.com>
2016-10-15 19:46:11 +0200

    Hast sortiert

--- Ruby_Auslese.rb
+++ Ruby_Auslese.rb
@@ -1,6 +1,6 @@
 
 counter = 1
-file = File.new("Daten/pegida_korpus_short.csv", "r")
+file = File.new("Daten/pegida_korpus.csv", "r")
 $woerter = {}
 $regexp_url = /((http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(([0-9]{1,5})?\/.*)?)/ix
 $regexp_kein_buchstabe = /\p{^Letter}/u
@@ -9,11 +9,13 @@ def belegen(wort)
   if $woerter.key?(wort)
     #puts "if " + wort
       neu = $woerter[wort]
-      neu = neu++
+      neu = neu + 1
       $woerter[wort] = neu
+      #puts neu
+      #puts $woerter[wort]
   else
     #puts "else " + wort
-    $woerter[wort]= 1
+    $woerter[wort] = 1
   end
 end
   while (line = file.gets)
@@ -23,14 +25,15 @@ end
             timestamp = line[0, 24]
             post = post
             .gsub(/ +/, " ") #leerzeichen zu einem zusammenfassen
-            puts post
             #puts timestamp
+            #puts post
             values = post.split(" ")
             values.each do |wort|
               belegen(wort)
             end
             counter = counter + 1
   end
-
 #puts $woerter
 file.close
+$woerter_sortiert = $woerter.sort {|a1, a2| a1[1].to_i <=> a2[1].to_i}
+puts $woerter_sortiert
