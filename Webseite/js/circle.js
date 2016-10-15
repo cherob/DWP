2de820c15e6ce1ef40a577ecd83dd1be0852cfce
Daniela Berger <danielaKay@nordlicht-development.de>
2016-10-15 15:12:22 +0200

    durch die Datenquelleiterieren

--- Webseite/js/circle.js
+++ Webseite/js/circle.js
@@ -1,4 +1,4 @@
-	$( document ).ready(function() {
+$( document ).ready(function() {
 //Paste here coode:
 	
 	var mode = "An";
@@ -7,32 +7,38 @@
 	var g = 255;
 	var b = 1;
 	var returnVar = false;
-	var runing = true;
+	var running = true;
 	
 	$( "#button" ).on( "click", function() {
-  if(runing == true){
-	runing = false;
-	mode = "Aus";
-  }else{
-	runing = true;
-	mode = "An";
-  }
-});
+		if(running == true){
+			running = false;
+			mode = "Aus";
+  		} else {
+			running = true;
+			mode = "An";
+  		}
+	});
 	
 	function getMode() {
 		
 	}
 	
-   console.log( "ready!" );
+   	console.log( "ready!" );
 
-	
+	console.log(amountData.eins);
+
+	$.each(amountData, function( index, value ) {
+  		console.log( index + " => " + value );
+	});
+
+
 	makeCircle();
 
 	function makeCircle() {
-if(runing){
-	myPanel = new jsgl.Panel(document.getElementById("panel"));
+		if(running){
+			myPanel = new jsgl.Panel(document.getElementById("panel"));
 
-/*	console.log(getAmount);
+/*	console.log(amountData);
 	if(r>127){
 	g++;	
 	b--;}
@@ -46,28 +52,28 @@ if(runing){
 	 //circle.getStroke().setColor("rgb("+r+","+g+","+b+")");
 	*/
 	
-	circle = myPanel.createCircle();
-	circle.setCenterLocationXY(400,300);
+			circle = myPanel.createCircle();
+			circle.setCenterLocationXY(400,300);
+			
+			circle.setRadius(amountData.eins);
+			circle.getStroke().setWeight(10);
+			myPanel.addElement(circle);
+			
+			
+			if(radius==250){
+				returnVar = true;
+			}else if(radius==0){
+				returnVar = false;
+			}
 	
-	circle.setRadius(getAmount.eins);
-	circle.getStroke().setWeight(10);
-	myPanel.addElement(circle);
 	
-	
-	if(radius==250){
-		returnVar = true;
-	}else if(radius==0){
-		returnVar = false;
-	}
-	
-	
-		if(returnVar == false){
-			radius++;
-		}else{
-			radius--;
+			if(returnVar == false){
+				radius++;
+			}else{
+				radius--;
+			}
 		}
-	}
-	window.setTimeout(makeCircle,500);
+		window.setTimeout(makeCircle,500);
 	
-}
-	});
+	}
+});
